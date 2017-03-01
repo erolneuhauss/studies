16ff5e31114c94010961aac7d8df57ae0fb8c175
erolneuhauss <eneuhauss@gmx.de>
2017-03-01 18:57:18 +0000

    fix minor error in comment

--- bash/for-loop-rename-file.sh
+++ bash/for-loop-rename-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# rename all files in current directory 
-# accomplish by search for java and replace by jdk
+# rename all files in current directory
+# accomplished by search for jdk and replace by java
 
 for i in *; do
     mv "${i}" "${i/jdk/java}"
