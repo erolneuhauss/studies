258a6080d0451f051c51307a1838eb5582f6ef7c
erolneuhauss <erol.neuhauss@gmx.de>
2017-03-22 13:06:53 +0100

    not only start and provision parallel, but also halt

--- ansible/start_vb_parallel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-vagrant up master --provision &
-vagrant up slave1 --provision &
-vagrant up slave2 --provision &
-vagrant up slave3 --provision &
-
---
+++ ansible/vagrant_parallel_start-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ARG="$1"
+
+case "$ARG" in
+  up) 
+		vagrant up master --provision &
+		vagrant up slave1 --provision &
+		vagrant up slave2 --provision &
+		vagrant up slave3 --provision
+		;;
+  halt)
+		vagrant halt slave3 &
+		vagrant halt slave2 &
+		vagrant halt slave1 &
+		vagrant halt master
+		;;
+	*)
+    echo $"Usage: $0 {up|halt}"
+esac
