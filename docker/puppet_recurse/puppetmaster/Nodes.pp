ad14ab91553ef6786daeb9b58a3dc689d200a6bf
erolneuhauss <erol.neuhauss@gmx.de>
2017-05-11 19:46:47 +0200

    renaming services/docker instances

--- docker/puppet_recurse/puppetmaster/Nodes.pp
+++ docker/puppet_recurse/puppetmaster/Nodes.pp
@@ -1,11 +1,11 @@
-puppet {
+client {
 
   file { '/dataset_5000_files' :
     ensure  => directory,
     owner   => root,
   }
 
-  file { '/tmp/erol_puppet':
+  file { '/tmp/erol_client.txt':
     ensure => present,
   }
 
