dcafa3e362494203b7575ecc4b3b89720026a1a6
erolneuhauss <erol.neuhauss@gmx.de>
2017-05-22 16:22:41 +0200

    try to tidy up

--- puppet/provision/logoutput/config/puppetmaster/etc/puppet/environments/production/manifests/Nodes.pp
+++ puppet/provision/logoutput/config/puppetmaster/etc/puppet/environments/production/manifests/Nodes.pp
@@ -1,14 +1,35 @@
 node 'client' {
 
-  exec { 'list long state.yaml' :
-    command   => '/bin/ls -lh /var/lib/puppet/state/state.yaml',
-    logoutput => true,
-    path      => ['/bin','/usr/bin','/usr/sbin','/sbin'],
-    require   => File['/var/lib/puppet/state/state.yaml'],
+  $statedir = '/var/lib/puppet/state'
+  $stateyaml = '/var/lib/puppet/state/state.yaml'
+
+  file { $stateyaml:
+    ensure  => present,
   }
 
-  file { '/var/lib/puppet/state/state.yaml':
+  exec { 'list long state.yaml' :
+    command   => "/bin/ls -lh ${stateyaml}",
+    logoutput => true,
+    path      => ['/bin','/usr/bin','/usr/sbin','/sbin'],
+    require   => File[$stateyaml],
+  }
+
+  exec { 'cp state.yaml to temp' :
+    command => "/bin/cp ${stateyaml} /tmp",
+    path    => ['/bin','/usr/bin','/usr/sbin','/sbin'],
+    require => File[$stateyaml],
+  }
+
+  file { '/tmp/state.yaml':
     ensure  => present,
+    mode    => '0644',
+    require => Exec['cp state.yaml to temp'],
+  }
+
+  tidy { $statedir:
+    size    => '3k',
+    recurse => 1,
+    matches => 'state.yaml'
   }
 
 }
