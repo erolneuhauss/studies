cda7fd1896fe688845c9c739489090a2c49a75b1
erolneuhauss <erol.neuhauss@gmx.de>
2017-07-03 19:08:24 +0200

    try out puppet 5.0

---
+++ puppet/provision/puppet5/config/puppetmaster/etc/puppet/environments/production/manifests/Nodes.pp
@@ -0,0 +1,14 @@
+node 'client' {
+
+  file { '/dataset/dataset_10000_files' :
+    ensure       => directory,
+    owner        => root,
+    recurse      => true,
+    recurselimit => 1,
+  }
+
+  file { '/tmp/erol_client.txt':
+    ensure => present,
+  }
+
+}
