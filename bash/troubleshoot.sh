9545244d4c0fa56e1a8573cf506a1c966d863834
eneuhauss <erol.neuhauss@gmx.de>
2021-09-16 16:01:17 +0200

    break and continue

--- bash/troubleshoot.sh
+++ bash/troubleshoot.sh
@@ -4,15 +4,31 @@
 
 # set -x
 
-read -p "Enter a filename to read: " FILE
+# read -p "Enter a filename to read: " FILE
+FILE="super.txt"
 
 COUNT=1
 
+echo "first while iteration "
+while read -r SUPERHERO; do
+  echo "Superhero name: $SUPERHERO"
+done < "${FILE}"
+printf "\n"
+
+echo "second while iteration with break"
+while read -r SUPERHERO; do
+  echo "Superhero name: $SUPERHERO"
+  break
+done < "${FILE}"
+printf "\n"
+
+echo "third while iteration with count to 3"
 while read -r SUPERHERO; do
   if [[ $COUNT = 3 ]]; then
     continue
   else
-    COUNT+=1
+    let COUNT+=1
   fi
   echo "Superhero name: $SUPERHERO"
 done < "${FILE}"
+
