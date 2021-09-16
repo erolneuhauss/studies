6c626fb95be902839e43c398e7f3ff04994a1dc6
eneuhauss <erol.neuhauss@gmx.de>
2021-09-16 10:26:44 +0200

    Debug part 1

--- bash/heredoc_herestring.sh
+++ bash/heredoc_herestring.sh
@@ -2,6 +2,19 @@
 # https://learn.acloud.guru/course/admin-guide-to-bash-scripting/dashboard
 # The System Administrator’s Guide to Bash Scripting - NEW 2020
 
+# exits immediatly, if anything exits with a non-zero status
+set -e
+# read commands but does not execute them. For testing purposes
+# set -n
+# display expanded values. works like strace
+# set -x
+
+# exit with an error, if variables are not set
+set -u
+ARG1=$1
+ARG2=$2
+# without arguments, set -u will make script exit with a message
+
 cat << "EOF"
 no variable expansion
 $USER
@@ -17,19 +30,19 @@ cat <<- EOF
 EOF
 
 STRING="This is a string of words in a variable."
-
 read -r -a Words <<< $STRING
+echo ${STRING[*]}
 
 echo "1. word is '${Words[0]}'"
 echo "2. word is '${Words[1]}'"
 echo "3. word is '${Words[2]}'"
 echo "4. word is '${Words[3]}'"
 
-
 ARRAY=(e1 e2 e3 {A..F})
 echo "Iterate over an array with a while loop"
 while read element; do
   echo $element
+  echo "read builtin is reading the entire array at once"
 done <<< $(echo ${ARRAY[*]})
 
 echo "Iterate over an array with a for loop"
