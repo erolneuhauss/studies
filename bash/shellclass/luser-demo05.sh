12a5fe95d07d88cdb84102976577e3f5cec05b11
eneuhauss <erol.neuhauss@gmx.de>
2021-09-17 20:57:01 +0200

    last RANDOM excercise

--- bash/shellclass/luser-demo05.sh
+++ bash/shellclass/luser-demo05.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script generates a list a random passwords
-# but not so secure ones for practice 
+# but not so secure ones for practice
 
 # A random number as a passwords
 PASSWORD="${RANDOM}"
@@ -18,5 +18,10 @@ echo "${PASSWORD}"
 # A little better password
 PASSWORD=$(date +%s%N${RANDOM} | sha256sum | head -c32)
 echo "${PASSWORD}"
+
+# last way to generate a password
+# Append a special character to the password
+SPECIAL_CHAR=$(echo '!@#$%^&**(_)-=+?><,./;[]{}' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHAR}"
 
 exit 0
