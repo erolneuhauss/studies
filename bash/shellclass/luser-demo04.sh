8dc97a9ed68ecc2ea18345f4ae54f444e3a08a78
eneuhauss <erol.neuhauss@gmx.de>
2021-09-17 15:41:23 +0200

    more practice with bad usage of sudo

--- bash/shellclass/luser-demo04.sh
+++ bash/shellclass/luser-demo04.sh
@@ -2,6 +2,22 @@
 
 # This script creates an account on the local system
 # You will be prompted for the account name and password
+# In order to practice if-conditions, I added the sudo requirement
+# inside this script in opposite to the good practice to run sudo
+# outside the script, e.g. sudo ./luser-demo04.sh
+
+# Package sudo needs to be installed in order this to work
+SUDO_INSTALLED=$(rpm -qa | grep -q sudo)
+if [[ "${SUDO_INSTALLED}" -ne "0" ]]; then
+  echo "This script requires sudo package to be installed."
+fi
+
+# Has the user sudo rights and does he has to provide a password
+USER_HAS_SUDO=$(sudo -l -U | grep -q -P "NOPASSWD:\s+?ALL")
+if [[ "${USER}" -ne "vagrant" || "${USER_HAS_SUDO}" -ne "0" ]]; then
+  echo "NOPASSWD sudo powers required to run this script"
+  exit 1
+fi
 
 echo "We will be creating a user. Please supply your information."
 
@@ -22,7 +38,7 @@ while [[ "${USER_EXISTS}" = "true" ]]; do
     read -p "Type the real name: " REAL_NAME
 
     # Create the user
-    sudo useradd -c "${REAL_NAME}" ${USER_NAME}
+    sudo useradd -c "${REAL_NAME}" -m ${USER_NAME}
   fi
 done
 
