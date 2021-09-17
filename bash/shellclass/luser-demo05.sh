76d53eeb78ee35d110466667b0f50c05078f085f
eneuhauss <erol.neuhauss@gmx.de>
2021-09-17 20:45:41 +0200

    more password generators

--- bash/shellclass/luser-demo05.sh
+++ bash/shellclass/luser-demo05.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script generates a list a random passwords
+# but not so secure ones for practice 
 
 # A random number as a passwords
 PASSWORD="${RANDOM}"
@@ -8,7 +9,14 @@ echo "${RANDOM}"
 
 # Three random numbers together
 PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
+echo "${PASSWORD}"
 
+# A better password
+PASSWORD=$(date +%s%N | sha256sum | head -c32)
+echo "${PASSWORD}"
+
+# A little better password
+PASSWORD=$(date +%s%N${RANDOM} | sha256sum | head -c32)
 echo "${PASSWORD}"
 
 exit 0
