4c8a4994d185dd460c758ba19d1fb2f966b72b4a
eneuhauss <erol.neuhauss@gmx.de>
2021-09-21 16:18:26 +0200

    introducing case statement

--- bash/shellclass/luser-demo09.sh
+++ bash/shellclass/luser-demo09.sh
@@ -2,8 +2,33 @@
 
 # This script demonstrages the case statement
 
-if [[ "${1}" = 'start' ]]; then
-  echo starting
-elif [[ "${1}" = 'stop' ]]; then
-  echo stopping
-fi
+# action dependend of user statement via if
+# if [[ "${1}" = 'start' ]]; then
+#   echo 'Starting'
+# elif [[ "${1}" = 'stop' ]]; then
+#   echo 'Stopping'
+# elif [[ "${1}" = 'status' ]]; then
+#   echo 'Status: '
+# else
+#   echo 'Supply a valid option.'
+#   exit 1
+# fi
+
+# instead of using if, use case
+case "${1}" in
+  start)
+    echo 'Starting'
+    ;;
+  stop)
+    echo 'Stopping'
+    ;;
+  status|state|--status|--state)
+    echo 'Status:'
+    ;;
+  *)
+    echo 'Supply a valid option.'
+    exit 1
+    ;;
+esac
+
+exit 0
