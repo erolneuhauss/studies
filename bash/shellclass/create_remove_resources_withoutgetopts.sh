2cf8fde5f3601936f4d1f1160b9a8ee6cacb845a
eneuhauss <erol.neuhauss@gmx.de>
2021-11-09 20:08:46 +0100

    This variant without getops expects mandatory and accepts optional arguments

--- bash/shellclass/create_remove_resources_withoutgetopts.sh
+++ bash/shellclass/create_remove_resources_withoutgetopts.sh
@@ -7,73 +7,91 @@
 # -u checks for unset variables
 set -eu
 
-# always initiate variables, especially when working with options
-# otherwise major area of bugs
-ACTION="${1:-create}"
-TYPE="${2:-cluster}"
-NAME="${3:-stage}"
-THIS_SCRIPT="$(basename "${BASH_SOURCE[0]}")"
-
 usage() {
-  echo "Usage: ${0} ACTION TYPE NAME" >&2
+  echo "Usage: ${0} ACTION TYPE CLUSTER_NAME [NAMESPACE1] [NAMESPACE2]..." >&2
   echo ''
-  echo 'Creates, deploys or removes defined resources'
-  echo 'ARGS must be passed in exact order: ACTION TYPE NAME'
+  echo 'Creates or removes defined resources'
+  echo 'ARGS must be passed in exact order: ACTION TYPE CLUSTER_NAME'
   echo ''
-  echo '  ACTION can be one of: create|deploy|remove'
-  echo '  TYPE can be one of:   app|cluster|ns'
-  echo '  NAME should begin with a character [a-zA-Z]'
+  echo '  ACTION can be one of: create|remove'
+  echo '  TYPE can be one of:   cluster|ns'
+  echo '  Any CLUSTER_NAME or NAMESPACES should begin with a alpabetic character [a-zA-Z]'
   return 1
 }
 
 create_resources() {
-  echo "Creating '${TYPE}' named '${NAME}'."
+  echo "ACTION: ${*}"
+  if [[ "${#}" -gt 3 ]]; then
+    shift 3
+    i=1
+    echo "NAMESPACES: "
+    for args in "${@}"; do
+      echo "${i}. Namespace to create: ${args}"
+      (( i+=1 ))
+    done
+  fi
 }
 
 remove_resources() {
   echo "Remove '${TYPE}' named '${NAME}'."
 }
 
-# Inspect OPTIND
-echo "Number of args: ${#}"
+# Show info about arguments
 set +u
+echo "Number of args: ${#}"
 echo "All args: ${*}"
-set -u
 echo "1st arg: ${1:-}"
 echo "2nd arg: ${2:-}"
 echo "3rd arg: ${3:-}"
+echo "4rd arg: ${4:-}"
+echo "5rd arg: ${5:-}"
+echo "6rd arg: ${6:-}"
+set -u
 
-echo "ACTION: ${ACTION}"
-echo "NAME: ${NAME}"
-echo "TYPE: ${TYPE}"
+echo '#########################################'
+echo '############ BEGIN MAIN SCRIPT ##########'
+echo '#########################################'
 
-
-# superfluous arguments should be noted.
-if [[ "${#}" -gt 3 ]]; then
-  echo "You provided at some point an extra argument.
-  You provided: ${THIS_SCRIPT} ${*}
-  Expected: $THIS_SCRIPT ACTION TYPE NAME"
-  usage
-fi
-
-# check for valid user input
-if [[ ! "${ACTION}" =~ create|deploy|remove ]]; then
-  echo "Provide a valid ACTION. Expecting: 'create|deploy|remove'
-  Otherwise I don't know what to do"
-  usage
-elif [[ -z "${TYPE}" ]] || [[ ! "${TYPE}" =~ app|ns|cluster ]]; then
-  echo "Provide a valid TYPE. Expecting: 'app|ns|cluster'"
-  usage
-elif [[ -z "${NAME}" ]]; then
-  echo "Provide a valid NAME. Expecting: 'NAME', e.g. 'stage'"
-  usage
+# MAIN SCRIPT
+## WITHOUT ANY ARGUMENTS JUST CREATE DEFAULT CLUSTER
+if [[ "${#}" -lt 1 ]]; then
+  ACTION="default"
+## EXAMINE INPUT
+elif [[ "${1:-}" == 'help' ]]; then
+  ACTION='help'
+## EXAMINE EXISTENCE AND VALIDITY OF MANDATORY ARGUMENTS
+elif [[ ! "${1:-}" =~ help|create|remove ]]; then
+  echo "Provide a valid ACTION. Expecting: 'help|create|remove'"
+  exit 1
+elif [[ ! "${2:-}" =~ ns|cluster ]]; then
+  echo "Provide a valid TYPE. Expecting: 'ns|cluster'"
+  exit 1
+elif [[ -z "${3:-}" ]]; then
+  echo "Provide a valid NAME for the cluster. Expecting: 'STRING'"
+  exit 1
 else
-  case "${ACTION}" in
-    create|deploy)
-      create_resources "${TYPE}" "${NAME}"
-      ;;
-    remove)
-      remove_resources "${TYPE}" "${NAME}"
-      ;;
-  esac
+  ACTION="${1}"
+  TYPE="${2}"
+  CLUSTER_NAME="${3}"
+  ### IF ACTION on TYPE 'ns' WE NEED A LEAST A 4TH ARGUMENT
+  if [[ "${TYPE}" == 'ns' && "${#}" -le 3 ]]; then
+    echo "Provide a valid NAME for the namespace in cluster ${CLUSTER_NAME}.
+    Expecting at least 1 'STRING' or whitespaced strings: 'STRING1' 'STRING2'..."
+    exit 1
+  fi
 fi
+
+case "${ACTION}" in
+  default)
+    create_resources "create" "cluster" "stage"
+    ;;
+  create)
+    create_resources "${@}"
+    ;;
+  remove)
+    remove_resources "${TYPE}" "${CLUSTER_NAME}"
+    ;;
+  help)
+    echo "ACTION: ${ACTION}"
+    ;;
+esac
